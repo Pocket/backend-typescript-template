a094fd4dede22c20d6d181faacfc2b6cd108db4d
Daniel Brooks <bass_rock@me.com>
2020-06-26 15:35:05 -0700

    setting up eslint rc

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,7 @@
 export default {
-    sentry: {
-      dsn: process.env.SENTRY_DSN || '',
-      release: process.env.GIT_SHA || '',
-      environment: process.env.NODE_ENV || 'development',
-    },
-  };
-  
+  sentry: {
+    dsn: process.env.SENTRY_DSN || '',
+    release: process.env.GIT_SHA || '',
+    environment: process.env.NODE_ENV || 'development',
+  },
+};
